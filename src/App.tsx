2f307128a3992edd466fc365a088ff62785c142a
amhed <aherrera@gmail.com>
2021-08-21 10:42:31 -0500

    Demo on using google maps Stop exposing API Keys

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import CampaignAdmin from './CampaignsAdmin/CampaignAdmin';
 import { CampaignDetails } from './CampaignsAdmin/CampaignDetails';
 import 'antd/dist/antd.css';
 import './index.css';
+import { MapDemo } from './MapDemo';
 const { Header, Content, Footer } = Layout;
 function App() {
   return (
@@ -39,6 +40,8 @@ function App() {
               <Route path="/LocationsAdmin" exact component={LocationsAdmin} />
               <Route path="/CampaignsAdmin" exact component={CampaignAdmin} />
               <Route path="/CampaignsAdmin/:id" component={CampaignDetails} />
+              {/* TODO: Amhed: Update */}
+              {/* <MapDemo center={{ lat: 59.95, lng: 30.33 }} zoom={11} /> */}
             </Switch>
           </div>
 
--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,11 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 export const settings = {
   supabase: {
     uri: process.env.REACT_APP_MAGIC_BOOST_SUPABASE_URI!,
     apiKey: process.env.REACT_APP_MAGIC_BOOST_SUPABASE_API_KEY!,
   },
+  googleMaps: {
+    apiKey: process.env.REACT_APP_MAGIC_BOOST_GOOGLE_MAPS_API!
+  }
 };
+
