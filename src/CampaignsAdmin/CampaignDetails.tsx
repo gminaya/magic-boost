f0056698fad516b6c4462c057347df650642ac13
Gabriel Minaya <minaya001@gmail.com>
2021-08-26 20:32:25 -0500

    New component to handle report status Added more text

--- src/CampaignsAdmin/CampaignDetails.tsx
+++ src/CampaignsAdmin/CampaignDetails.tsx
@@ -4,16 +4,34 @@ import { settings } from '../settings';
 import { definitions } from '../db/supabase';
 import { useParams } from 'react-router-dom';
 import { CampaignLocationInfo } from '../models/CampaignLocationInfo';
+import { Table, Divider } from 'antd';
+import { DueDateStatus } from './DueDateStatus';
 
 export function CampaignDetails() {
-  const [location, setLocation] = useState<string>();
+  const [locationName, setLocationName] = useState<string>();
   const [dueDate, setDueDate] = useState<Date>();
-  const [campaignLocationInfo, setCampaignLocationInfo] = useState<CampaignLocationInfo>();
+  const [campaignLocationInfo, setCampaignLocationInfo] = useState<CampaignLocationInfo[]>();
   type CampaignParams = {
     id: string;
   };
-
   const { id } = useParams<CampaignParams>();
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'adress',
+    },
+    {
+      title: 'Photo',
+      dataIndex: 'photoURL',
+      key: 'photoURL',
+    },
+  ];
 
   useEffect(() => {
     getCampaignDetails();
@@ -24,22 +42,32 @@ export function CampaignDetails() {
     const supabase = createClient(uri, apiKey);
 
     //TODO: Encapsulate into its own hook?
-    const { data } = await supabase
-      .from<definitions['Campaigns']>('Campaigns')
-      .select()
-      .eq('id', Number(id))
-      .single();
+    const { data } = await supabase.from<definitions['Campaigns']>('Campaigns').select().eq('id', Number(id)).single();
 
     if (data !== null) {
-      setLocation(data.name);
+      setLocationName(data.name);
       setDueDate(data.dueDate as any);
       setCampaignLocationInfo(JSON.parse(data.location_config || ''));
     }
   };
-
   return (
     <>
-      <h1> {location} </h1>;
+      <Divider orientation={'left'}>
+        <span>You are seeing the campaign locations info of:</span>
+        <h1> {locationName} </h1>
+      </Divider>
+
+      <span>
+        The status of this report is <DueDateStatus reportDueDate={dueDate} />
+      </span>
+      <Table
+        size={'small'}
+        style={{ margin: 5 }}
+        bordered
+        loading={campaignLocationInfo == null}
+        dataSource={campaignLocationInfo}
+        columns={columns}
+      />
     </>
   );
 }
