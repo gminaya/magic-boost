e2fa542f4dc795347b2687d1ee7b6d141638ff6b
Gabriel Minaya <minaya001@gmail.com>
2021-09-22 15:05:29 -0500

    CampaignReport file creaed

---
+++ src/CampaignsAdmin/CampaignReport.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const CampaignReport = () => {
+  return (
+    <div className="location-card">
+      <header>
+        <h3>Location Name</h3>
+        <span>Location direction details</span>
+      </header>
+    </div>
+  );
+};
