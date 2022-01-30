eaef99b841473ea932796ab33428c7867c6bd7d6
amhed <aherrera@gmail.com>
2022-01-30 15:19:45 -0400

    Streamline instantiation of supabase client

--- src/db/helper.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import { settings } from 'settings';
-
-const { uri, apiKey } = settings.supabase;
-export const supabase = createClient(uri, apiKey);
