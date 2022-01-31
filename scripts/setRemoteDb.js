8f12e5cb22908771f77ba99c335493d463ddec61
amhed <aherrera@gmail.com>
2022-01-31 09:39:37 -0400

    Script now reads correct env variable

--- scripts/setRemoteDb.js
+++ scripts/setRemoteDb.js
@@ -1,7 +1,12 @@
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-require('dotenv').config();
+console.log(process.env.NODE_ENV);
+const environment = ['production', 'staging'].includes(process.env.NODE_ENV)
+  ? `.env.${process.env.NODE_ENV}`
+  : '.env';
+
+require('dotenv').config({ path: environment });
 const { exec } = require('./execWrapper');
 
 const connString = process.env.MAGIC_BOOST_CONNECTION_STRING;
