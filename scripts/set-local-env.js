c3b6bf78550d626bef213072032049cd0f42f068
amhed <aherrera@gmail.com>
2022-02-07 11:38:00 -0400

    Code complete

--- scripts/set-local-env.js
+++ scripts/set-local-env.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
-const { getActiveEnv, setActiveEnv } = require('./execWrapper');
+const { getActiveEnv, setActiveEnv, exec } = require('./execWrapper');
 
 const environment = process.argv[2];
 if (!(['dev', 'staging', 'production']).includes(environment)) {
   throw Error('Invalid environment')
 }
 
+// update buildConfig.js
 setActiveEnv(environment);
 
-//scripts/setRemoteDb.js
-//node -r dotenv/config scripts/download-types.js dotenv_config_path=.env.staging
+// update supabase config
+const dotEnvConfigPath = ['production', 'staging'].includes(environment)
+  ? `.env.${environment}`
+  : '.env';
+
+dotEnvConfig = require('dotenv').config({ path: dotEnvConfigPath }).parsed;
+exec(`supabase db remote set ${dotEnvConfig.MAGIC_BOOST_CONNECTION_STRING}`)
+
